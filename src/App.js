af82ccbb4c0da6b8e99086ffab9fbfdf755a935f
IngaD89 <inga.demetrashvili.1@gmail.com>
2021-10-28 11:43:41 +0200

    making components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Form from "./components/Form";
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Router>
+        <NavBar/>
+        <Switch>
+            <Route exact path="/">
+                <Home/>
+            </Route>
+           </Switch>
+        <Switch>
+            <Route exact path="/form">
+                <Form/>
+            </Route>
+        </Switch>
+    </Router>
+
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div>The page is on construction</div>
+    )
+}
